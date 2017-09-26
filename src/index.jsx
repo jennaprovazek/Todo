b54652217c29383f429c63e8132745543fa469aa
jennaprovazek <jennaprovazek@yahoo.com>
2017-09-26 16:26:21 -0700

    working version with everything running

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-// Application entrypoint.
+/*// Application entrypoint.
 
 // Load up the application styles
 require("../styles/application.scss");
@@ -9,3 +9,21 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
 ReactDOM.render(<App />, document.getElementById('react-root'));
+*/
+
+
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
+
+let store = createStore(todoApp)
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
