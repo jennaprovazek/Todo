01951a069828731af590a41faf3953f64134327f
jennaprovazek <jennaprovazek@yahoo.com>
2017-09-26 15:56:38 -0700

    Initial components migrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import React, {Component} from 'react';
+/*import React, {Component} from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      message: 'Hello React :)'
+    }
+  }
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <h1 onClick={() => this.handleClick()}>{this.state.message}</h1>
     );
   }
+
+  handleClick() {
+      this.setState({message: 'test'})
+  }
 }
-export default App;
+export default App;*/
+
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
+
+let store = createStore(todoApp)
+
+render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+)
